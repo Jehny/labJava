f704dcaa7cd0361b27e0540aa70eda7a3091b5a3
Mayara Gouveia <35708820+gb8may@users.noreply.github.com>
2020-05-08 12:17:35 -0300

    Update FirstTest.java

--- src/test/java/com/br/FirstTest.java
+++ src/test/java/com/br/FirstTest.java
@@ -15,12 +15,15 @@ public class FirstTest {
 	public void browserChrome(){
 
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
+		WebDriver driver = new ChromeDriver(options);
 		driver.get(url);
-		if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-			const char kDisableDevShmUsage[] = "disable-dev-shm-usage";
-		endif
-
 	}
 	
 	//@Test
