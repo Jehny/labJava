aa94cf1bde9c310c89b6523f2b6f1e738ee28d0d
Mayara Gouveia <35708820+gb8may@users.noreply.github.com>
2020-05-08 11:48:28 -0300

    Update FirstTest.java

--- src/test/java/com/br/FirstTest.java
+++ src/test/java/com/br/FirstTest.java
@@ -14,7 +14,7 @@ public class FirstTest {
 	@Test
 	public void browserChrome(){
 
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver --whitelisted-ips="0.0.0.0"");
 		WebDriver driver = new ChromeDriver();
 		driver.get(url);
 
