6097a02eff48fdb3526b36a403861e4ca4d69755
Mayara Gouveia <35708820+gb8may@users.noreply.github.com>
2020-05-08 12:18:47 -0300

    Update FirstTest.java

--- src/test/java/com/br/FirstTest.java
+++ src/test/java/com/br/FirstTest.java
@@ -14,15 +14,10 @@ public class FirstTest {
 	@Test
 	public void browserChrome(){
 
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
-		WebDriver driver = new ChromeDriver(options);
+		System.setProperty(
+		"webdriver.chrome.driver",
+		"/usr/bin/chromedriver");
+		driver = new ChromeDriver();
 		driver.get(url);
 	}
 	
