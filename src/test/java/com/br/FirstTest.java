17c5a61b7a175913b10232be2c2a9ad8c5341867
Mayara Gouveia <35708820+gb8may@users.noreply.github.com>
2020-05-07 23:15:33 -0300

    Update FirstTest.java

--- src/test/java/com/br/FirstTest.java
+++ src/test/java/com/br/FirstTest.java
@@ -5,8 +5,10 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
 
 public class FirstTest {
 	protected WebDriver driver;
