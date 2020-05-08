19434cd2e6048b2fce1308ac29d45778226f9894
Mayara Gouveia <35708820+gb8may@users.noreply.github.com>
2020-05-08 10:14:49 -0300

    Update FirstTest.java

--- src/test/java/com/br/FirstTest.java
+++ src/test/java/com/br/FirstTest.java
@@ -3,35 +3,24 @@ package com.br;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class FirstTest {
 	protected WebDriver driver;
 	public String url = "http://www.globo.com";
 	
-	//@Test
-	public void browserIE() {
-		System.setProperty("webdriver.ie.driver",
-				"drivers\\IEDriverServer4.0.exe");
-		driver = new InternetExplorerDriver();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get(url);
-	}
-	
-	//@Test
+	@Test
 	public void browserChrome(){
 
-		System.setProperty(
-		"webdriver.chrome.driver",
-		"drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get(url);
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.globo.com");
 
 	}
 	
-	@Test
+	//@Test
 	public void browserFirefox(){
 		System.setProperty(
 				"webdriver.gecko.driver",
