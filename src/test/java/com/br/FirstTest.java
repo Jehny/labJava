fa887853ab4749fd87111b9f697e3aeebb978130
Jehnyffen <milliam.jehnyffen@hp.com>
2020-07-21 14:51:45 -0300

    add yaml files and docker file

--- src/test/java/com/br/FirstTest.java
+++ src/test/java/com/br/FirstTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -21,18 +22,29 @@ public class FirstTest {
 		driver.get(url);
 	}
 	
-	//@Test
+	@Test
 	public void browserChrome(){
-
+		System.out.println("Test started...");
 		System.setProperty(
-		"webdriver.chrome.driver",
-		"drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get(url);
+				"webdriver.chrome.driver",
+				"/usr/bin/chromedriver");
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--no-sandbox");
+				options.addArguments("--headless");
+				options.addArguments("--disable-dev-shm-usage");
+				options.addArguments("--whitelisted-ips");
+				driver = new ChromeDriver(options);
+				driver.get(url);
+		System.out.println("Test finished successfully!");
+//		System.setProperty(
+//		"webdriver.chrome.driver",
+//		"drivers\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get(url);
 
 	}
 	
-	@Test
+//	@Test
 	public void browserFirefox(){
 		System.setProperty(
 				"webdriver.gecko.driver",
